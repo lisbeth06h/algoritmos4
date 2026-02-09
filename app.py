e8f0e3c2727f8ec4fa980cba2837e31640a78abd
Lisbeth <lisbethherreraberrio.06@gmail.com>
2026-02-09 13:06:51 -0500

    Ejercicio lista doblemente ligada

---
+++ app.py
@@ -0,0 +1,116 @@
+#Listas doblemente ligadas
+
+class Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.anterior = None
+        self.siguiente = None
+
+class ListaDoble:
+    def __init__(self):
+        self.cabeza = None
+        self.cola = None
+    
+    def esta_vacia(self):
+        return self.cabeza is None
+    
+    def insertar_inicio(self, dato):
+        nuevo = Nodo(dato)
+
+        if self.esta_vacia():
+            self.cabeza = nuevo
+            self.cola = nuevo
+        else:
+            nuevo.siguiente = self.cabeza
+            self.cabeza = nuevo
+    
+    def insertar_final(self, dato):
+        nuevo = Nodo(dato)
+
+        if self.esta_vacia():
+            self.cabeza = nuevo
+            self.cola = nuevo
+        else:
+            self.cola.siguiente = nuevo
+            nuevo.anterior = self.cola
+            self.cola = nuevo
+
+    def eliminar_inicio(self):
+        if self.esta_vacia():
+            return None
+        if self.cabeza.dato == self.cola.dato:
+            self.cabeza = None
+            self.cola = None
+        else:
+            self.cabeza = self.cabeza.siguiente
+            self.cabeza.anterior = None
+    
+    def eliminar_final(self):
+        if self.esta_vacia():
+            return None
+        if self.cabeza.dato == self.cola.dato:
+            self.cabeza = None
+            self.cola = None
+        else:
+            self.cola = self.cabeza.siguiente
+            self.cola.siguiente = None
+    
+    def recorrer_adelante(self):
+        if self.esta_vacia():
+            print("Lista vacía")
+            return
+        
+        print("Recorriendo Inicio --> Fin")
+        actual = self.cabeza
+        while actual:
+            print(actual.dato, end=" -> ")
+            actual = actual.siguiente
+        print("Fin.")
+    
+    def recorrer_atras(self):
+        if self.esta_vacia():
+            print("Lista vacía")
+            return
+        
+        print("Recorriendo Fin --> Inicio")
+        actual = self.cola
+        while actual:
+            print(actual.dato, end=" -> ")
+            actual = actual.anterior
+        print("Inicio.")
+
+    def buscar(self, dato):
+        actual = self.cabeza
+        while actual:
+            if actual.dato == dato:
+                return True
+            actual = actual.siguiente
+        return False
+    
+    def __len__(self):
+        contador = 0
+        actual = self.cabeza
+        while actual:
+            contador += 1
+            actual = actual.siguiente
+        return contador
+    
+    def __str__(self):
+        if self.esta_vacia():
+            return "Lista vacia"
+
+        elementos = []
+        actual = self.cabeza
+        while actual:
+            elementos.append(str(actual.dato))
+            actual = actual.siguiente
+        return " <=> ".join(elementos)
+    
+lista = ListaDoble()
+lista.insertar_final(10)
+lista.eliminar_final(20)
+lista.eliminar_final(30)
+print(lista)
+
+lista.insertar_inicio(40)
+print(lista)
