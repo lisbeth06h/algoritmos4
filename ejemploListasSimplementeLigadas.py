42ece5880d44b63fdf026eed37f4df3f20917669
Lisbeth <lisbethherreraberrio.06@gmail.com>
2026-02-11 23:04:33 -0500

    Correción ejercicio 2

--- ejemploListasSimplementeLigadas.py
+++ ejemploListasSimplementeLigadas.py
@@ -4,48 +4,36 @@ class Nodo:
         self.cedula = cedula
         self.siguiente = None
         self.prioridad = prioridad
+
 class Lista:
     def __init__(self):
         self.cabeza = None
 
     def AgregarNodoAlFinal(self, nombre, cedula, prioridad):
-        nodo = Nodo(nombre, cedula)
-        if self.cabeza == None:
+        nodo = Nodo(nombre, cedula, prioridad)
+
+        if self.cabeza is None:
             self.cabeza = nodo
         else:
             actual = self.cabeza
-            while actual.siguiente != None:
+            while actual.siguiente:
                 actual = actual.siguiente
-            
             actual.siguiente = nodo
+
         print("Nodo agregado exitosamente.")
-    
+
     def MostraNodo(self):
         actual = self.cabeza
         while actual:
-            print(actual.nombre, end="==>")
-            print("_____")
+            print(actual.nombre, "==>", end=" ")
             actual = actual.siguiente
-        print("Fin.")   
+        print("Fin.")
 
-    def AgregarConPrioridad(self, nombre, cedula, prioridad):
-        nodo = Nodo(nombre, cedula, prioridad)     
-        if self.cabeza == None:
-            self.cabeza = nodo
-        elif self.cabeza.prioridad > prioridad:
-            nodo.siguiente = self.cabeza
-            self.cabeza = nodo
-        else:
-            actual = self.cabeza
-            while actual.siguiente and actual.siguiente.prioridad <= prioridad:
-                actual = actual.siguiente
-            nodo.siguiente = actual.siguiente
-            actual.siguiente = nodo
          
     
     
 lista = Lista()
-lista.AgregarNodoAlFinal("Juan", 111)
-lista.AgregarNodoAlFinal("Camila", 112)
-lista.AgregarNodoAlFinal("Pedro", 113)
+lista.AgregarNodoAlFinal("Juan", 111, 3)
+lista.AgregarNodoAlFinal("Camila", 112, 2)
+lista.AgregarNodoAlFinal("Pedro", 113,1)
 lista.MostraNodo()
