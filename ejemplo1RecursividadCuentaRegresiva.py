3b010e461db556d5c74a299eaa71bca3d797c5fb
Lisbeth <lisbethherreraberrio.06@gmail.com>
2026-02-17 22:20:29 -0500

    Ejemplo 1 de recursividad, cuenta regresiva.

---
+++ ejemplo1RecursividadCuentaRegresiva.py
@@ -0,0 +1,9 @@
+def cuenta_regresiva(n):
+    if n < 0: #condición de parada
+        print("Cuenta regresiva terminada")
+        return
+    print(n)
+    cuenta_regresiva(n - 1)#llamada recursiva con n-1 para acercarnos a la condicion base
+
+#ejemplo de uso
+cuenta_regresiva(10)
