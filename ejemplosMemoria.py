626da2be5e167133e22bb50a198492bb8358f25a
Lisbeth <lisbethherreraberrio.06@gmail.com>
2026-04-05 22:15:54 -0500

    Ejemplos de memorización

--- ejemplosMemoria.py
+++ ejemplosMemoria.py
@@ -33,3 +33,76 @@ def cambio(cantidad, moneda):
 
 monedas = [1, 5, 10, 25]
 print(cambio(50, monedas))  # Debería devolver 2 (10 +
+
+
+
+
+
+#{[()]}
+def verificar_expresion(expresion):
+    pila = []
+
+    pares = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    for caracter in expresion:
+        if caracter in "([{":
+            pila.append(caracter)
+
+        elif caracter in ")]}":
+            if not pila:
+                return False
+            
+            ultimo = pila.pop()
+
+            if pares[caracter] != ultimo:
+                return False
+
+    return len(pila) == 0
+
+
+
+def infija_a_postfija(expresion):
+    precedencia = {
+        '+': 1,
+        '-': 1, 
+        '*': 2, 
+        '/': 2
+    }
+    salida = []
+    pila = []
+
+    tokens = expresion.split()
+
+    for token in tokens:
+        if token.lstrip('-').replace('.', '', 1).isdigit():  # Verificar si es un número (incluyendo negativos y decimales)
+            salida.append(token)
+            print(f"{token} (número)-> se añade a la salida.")
+        elif token == '(':
+            pila.push(token)
+            print(f"{token} -> se añade a la pila.")
+        elif token == ')':
+            while not pila.esta_vacia() and pila.peek() != '(':
+                salida.append(pila.pop())
+            pila.pop()  # Eliminar el '(' de la pila
+            print(f"{token} -> se procesan los operadores hasta encontrar '('.")
+        elif token in precedencia:
+            while (not pila.esta_vacia() and pila.peek() != '(' and pila.peek() in precedencia and
+                 precedencia[pila.peek()] >= precedencia[token]):
+                salida.append(pila.pop())
+            pila.push(token)
+            print(f"{token} (operador) -> se procesa según la precedencia.")
+    while not pila.esta_vacia():
+        salida.append(pila.pop())
+    resultado = ' '.join(salida)
+    print(f"Expresión postfija: {resultado}")
+    
+infija_a_postfija("3 + 4 * 2 ")
+
+
+
+def demo_operaciones_basicas():
+    print("=" *60)
